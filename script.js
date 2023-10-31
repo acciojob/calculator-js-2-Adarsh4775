618aafc518739974fecce4c00e9ce2ef157878df
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-31 17:28:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+//your code here
+const input = document.getElementById('input');
+const buttons = document.querySelectorAll('.buttons button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (button.id === 'C') {
+            input.value = '';
+        } else if (button.id === 'equal') {
+            try {
+                input.value = eval(input.value);
+                if (input.value === Infinity) {
+                    input.value = 'Infinity';
+                } else if (isNaN(input.value)) {
+                    input.value = 'NaN';
+                }
+            } catch (error) {
+                input.value = 'Error';
+            }
+        } else {
+            input.value += button.textContent;
+        }
+    });
+});
